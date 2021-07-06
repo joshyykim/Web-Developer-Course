1ba3caca2a7956ac91b90e830cb5abf73036df4b
Suhun Kim <kksshh404@gmail.com>
2021-07-06 11:34:36 -0700

    commit

--- dom-practice/color_event/app.js
+++ dom-practice/color_event/app.js
@@ -2,12 +2,11 @@ const btn = document.querySelector('button')
 const head = document.querySelector('h2')
 
 const ranNum = function () {
-    let num = Math.floor((Math.random() * 255))
-    return num;
+    return Math.floor((Math.random() * 256))
 }
 
 btn.addEventListener('click', () => {
-    color = `rgb(${ranNum()},${ranNum()},${ranNum()})`
+    color = `rgb(${ranNum()}, ${ranNum()}, ${ranNum()})`
     document.body.style.backgroundColor = color;
     head.innerText = `${color}`
 });
